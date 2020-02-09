811d7fbf89f98feb3b7612fcc09cac5240ab0509
Sebastian Eschweiler <sebastian.eschweiler@gmail.com>
2020-02-09 10:41:33 +0100

    first commit

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Param, Post, Body, Delete, Query } from '@nestjs/common';
+import { CoursesService } from './courses.service';
+import { CreateCourseDto } from './create-course.dto';
+
+@Controller('courses')
+export class CoursesController {
+    constructor(private coursesService: CoursesService) {}
+
+    @Get()
+    async getCourses() {
+        const courses = await this.coursesService.getCourses();
+        return courses;
+    }
+
+    @Get(':courseId')
+    async getCourse(@Param('courseId') courseId) {
+        const course = await this.coursesService.getCourse(courseId);
+        return course;
+    }
+
+    @Post()
+    async addCourse(@Body() createCourseDto: CreateCourseDto) {
+        const course = await this.coursesService.addCourse(createCourseDto);
+        return course;
+    }
+
+    @Delete()
+    async deleteCourse(@Query() query) {
+        const courses = await this.coursesService.deleteCourse(query.courseId);
+        return courses;
+    }
+}
+
---
+++ src/courses/courses.mock.ts
@@ -0,0 +1,5 @@
+export const COURSES = [
+    { id: 1, title: 'NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)', description: "Master Node JS, build REST APIs with Node.js, GraphQL APIs, add Authentication, use MongoDB, SQL & much more!", author: 'Maximilian Schwarzmüller', url: 'https://codingthesmartway.com/courses/nodejs-complete-guide/' },
+    { id: 2, title: 'The Complete Web Developer in 2020: Zero to Mastery', description: "Learn to code and become a Web Developer in 2020 with HTML, CSS, Javascript, React, Node.js, Machine Learning & more!", author: 'Andrei Neagoie', url: 'https://codingthesmartway.com/courses/web-developer-2018/' },
+    { id: 3, title: 'Learn and Understand NodeJS', description: "Dive deep under the hood of NodeJS. Learn V8, Express, the MEAN stack, core Javascript concepts, and more.", author: 'Anthony Alicea', url: 'https://codingthesmartway.com/courses/learn-understand-nodejs/' },
+];
